443d5f078bacb17ae8f991f295af115997f8a28b
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-22 22:46:11 +0530

    toggling state on button click

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,17 @@
-<script>
+<script lang="ts">
 	import Greeting from './Greeting.svelte';
 
 	let name = $state('John');
+	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
+
+	function toggle() {
+		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
+	}
 </script>
 
 <Greeting {name} />
 
 <input type="text" bind:value={name} />
+
+<p>The store is now {status}</p>
+<button onclick={toggle}>Toggle status</button>
