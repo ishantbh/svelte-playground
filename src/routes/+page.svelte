24e9d67284d14af05335493c4312c07521e857e4
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-23 17:31:05 +0530

    template conditionals

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,48 @@
 <script lang="ts">
-	import Greeting from './Greeting.svelte';
-
-	let name = $state('John');
-	let status: 'OPEN' | 'CLOSED' = $state('OPEN');
-
-	let full_name = $derived(name + ' ' + 'Doe');
-
-	function onclick() {
-		status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
-	}
+	let formState = $state({
+		name: '',
+		birthday: '',
+		step: 0,
+		error: ''
+	});
 </script>
 
-<Greeting {name} />
+<main>
+	<p>Step: {formState.step + 1}</p>
 
-<h2>{full_name}</h2>
+	{#if formState.error}
+		<p class="error">{formState.error}</p>
+	{/if}
 
-<input type="text" bind:value={name} />
-
-<p>The store is now {status}</p>
-<button {onclick}>Toggle status</button>
+	{#if formState.step === 0}
+		<div>
+			<label for="name">Name</label>
+			<input type="text" id="name" bind:value={formState.name} />
+		</div>
+		<button
+			onclick={() => {
+				if (formState.name !== '') {
+					formState.step++;
+					formState.error = '';
+				} else {
+					formState.error = 'Name is required';
+				}
+			}}>Next</button
+		>
+	{:else if formState.step === 1}
+		<div>
+			<label for="bday">Your Birthday</label>
+			<input type="date" id="bday" bind:value={formState.birthday} />
+		</div>
+		<button
+			onclick={() => {
+				if (formState.birthday !== '') {
+					formState.step++;
+					formState.error = '';
+				} else {
+					formState.error = 'Birthday is required';
+				}
+			}}>Next</button
+		>
+	{/if}
+</main>
