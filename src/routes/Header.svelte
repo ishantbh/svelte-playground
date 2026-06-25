12c8f68e8456aab22ff788556f48739fcb204011
Ishant <269895395+ishantbh@users.noreply.github.com>
2026-06-25 14:12:19 +0530

    complex state in wrapper

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
+	import { createState } from './state.svelte';
+
 	const { name }: { name: string } = $props();
+
+	const myState = createState();
 </script>
 
 <div>
 	<h1>{name ? name : 'User'}&apos;s form</h1>
+	<!-- <button onclick={myState.up}>{myState.value}</button> -->
+	<button
+		onclick={() => {
+			myState.value = 10;
+		}}>{myState.value}</button
+	>
 </div>
 
 <style></style>
